82444245815feb7ffc2462e362fe19b9b8529748
Mahmood Elbadri <mahmoud.elbadry357@gmail.com>
2025-12-23 21:58:42 +0200

    updated token interceptor to handle 'Auth' keyword in URL instead of 'login' and 'register' paths

--- src/app/core/_interceptors/token.interceptor.ts
+++ src/app/core/_interceptors/token.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const tokenInterceptor: HttpInterceptorFn = (req, next) => {
-  if(req.url.includes('login') || req.url.includes('register')) return next(req);
+  if(req.url.includes('Auth')) return next(req);
   const token = localStorage.getItem('token');
   if(token) req = req.clone({setHeaders: {Authorization: `Bearer ${token}`}});
   return next(req);
